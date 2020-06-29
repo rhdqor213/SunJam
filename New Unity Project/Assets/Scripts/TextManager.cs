8ad189973e324dc4f5646dc34b624bcdc7167943
rhdqor213 <rhdqor213@naver.com>
2020-06-29 23:31:31 +0900

    대사 출력 간단 구현

---
+++ New Unity Project/Assets/Scripts/TextManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextManager : MonoBehaviour
+{
+    public Text namet;
+    public Text textt;
+    string[,,] ss = new string[,,] {
+        {
+            { "주인공", "가나" },
+            { "주인공", "다라" },
+            { "주인공", "마바" },
+            { "주인공", "사아" },
+        },
+    };
+    int n = 0, m = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        try
+        {
+            namet.text = ss[n, m, 0];
+            textt.text = ss[n, m, 1];
+        }
+       catch (System.IndexOutOfRangeException e)
+        {
+            new SceneChange().ToGame();
+        }
+    }
+
+    public void NextText()
+    {
+        m++;
+    }
+}
+/*
+대사
+
+*/
