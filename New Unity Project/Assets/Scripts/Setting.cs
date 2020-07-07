f6b03a68f842ed8506db575e525701b453cf60a5
rhdqor213 <rhdqor213@naver.com>
2020-07-08 02:39:05 +0900

    기본 층 구조 구현, 카메라 이동 위치 설정

---
+++ New Unity Project/Assets/Scripts/Setting.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Setting : MonoBehaviour
+{
+    int show = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnShow()
+    {
+        if (show == 0)
+        {
+            show = 1;
+            gameObject.SetActive(true);
+        }
+        else
+        {
+            show = 0;
+            gameObject.SetActive(false);
+        }
+    }
+}
