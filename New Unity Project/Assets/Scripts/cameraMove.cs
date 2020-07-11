2fda19cc4c8460cf9693a099427c10d8791c3c0a
rhdqor213 <rhdqor213@naver.com>
2020-07-11 19:41:41 +0900

    카메라 이동 관계 설정

--- New Unity Project/Assets/Scripts/cameraMove.cs
+++ New Unity Project/Assets/Scripts/cameraMove.cs
@@ -12,11 +12,13 @@ public class CamInfo
     public Vector3 pos;
     public Quaternion rot;
     public int[] mb;
-    public CamInfo(Vector3 _pos, Quaternion _rot, int[] _mb)
+    public bool[] block;
+    public CamInfo(Vector3 _pos, Quaternion _rot, int[] _mb, bool[] _block)
     {
         pos = _pos;
         rot = _rot;
         mb = _mb;
+        block = _block;
     }
 }
 
@@ -24,17 +26,31 @@ public class CameraMove : MonoBehaviour
 {
     // Start is called before the first frame update
     CamInfo[] ci = new CamInfo[] {
-        new CamInfo(new Vector3(410.3f, 2.722f, -311f), Quaternion.Euler(0f, 0f, 0f), new int[] {-1, -1, 3, 1}),
-        new CamInfo(new Vector3(409.22f, 2.722f, -310.57f), Quaternion.Euler(0f, 90f, 0f), new int[] {-1, -1, 0, 2}),
-        new CamInfo(new Vector3(410.3f, 2.722f, -310.18f), Quaternion.Euler(0f, 180f, 0f), new int[] {-1, -1, 1, 3}),
-        new CamInfo(new Vector3(410.94f, 2.722f, -310.57f), Quaternion.Euler(0f, 270f, 0f), new int[] {-1, -1, 2, 0}),
-        new CamInfo(new Vector3(409.58f, 2.19f, -308.87f), Quaternion.Euler(30f, -45f, 0f), new int[] {-1, 0, -1, -1}),
-        new CamInfo(new Vector3(409.383f, 2.453f, -312.523f), Quaternion.Euler(40f, 260f, 0f), new int[] {-1, 3, -1, -1}),
-        new CamInfo(new Vector3(410.65f, 2.258f, -310.879f), Quaternion.Euler(30f, 90f, 0f), new int[] {-1, 1, -1, -1}),
-        new CamInfo(new Vector3(412.26f, 2.722f, -305.16f), Quaternion.Euler(0f, 270f, 0f), new int[] {8, -1, -1, -1}),
-        new CamInfo(new Vector3(408.08f, 2.722f, -305.16f), Quaternion.Euler(0f, 270f, 0f), new int[] {-1, -1, -1, -1}),
+        new CamInfo(new Vector3(410.3f, 2.722f, -311f), Quaternion.Euler(0f, 0f, 0f), new int[] {-1, -1, 3, 1}, new bool[] {false, false, false, false}),
+        new CamInfo(new Vector3(409.22f, 2.722f, -310.57f), Quaternion.Euler(0f, 90f, 0f), new int[] {-1, -1, 0, 2}, new bool[] {false, false, false, false}),
+        new CamInfo(new Vector3(410.3f, 2.722f, -310.18f), Quaternion.Euler(0f, 180f, 0f), new int[] {-1, -1, 1, 3}, new bool[] {false, false, false, false}),
+        new CamInfo(new Vector3(410.94f, 2.722f, -310.57f), Quaternion.Euler(0f, 270f, 0f), new int[] {-1, -1, 2, 0}, new bool[] {false, false, false, false}),
+        new CamInfo(new Vector3(409.58f, 2.19f, -308.87f), Quaternion.Euler(30f, -45f, 0f), new int[] {-1, 0, -1, -1}, new bool[] {false, false, false, false}),
+        new CamInfo(new Vector3(409.383f, 2.453f, -312.523f), Quaternion.Euler(40f, 260f, 0f), new int[] {-1, 3, -1, -1}, new bool[] {false, false, false, false}),
+        new CamInfo(new Vector3(410.65f, 2.258f, -310.879f), Quaternion.Euler(30f, 90f, 0f), new int[] {-1, 1, -1, -1}, new bool[] {false, false, false, false}),
+        new CamInfo(new Vector3(412.26f, 2.722f, -305.16f), Quaternion.Euler(0f, 270f, 0f), new int[] {8, -1, -1, -1}, new bool[] {true, false, false, false}),
+        new CamInfo(new Vector3(408.08f, 2.722f, -305.16f), Quaternion.Euler(0f, 270f, 0f), new int[] {13, 19, -1, -1}, new bool[] {false, true, false, false}),
+        new CamInfo(new Vector3(405.99f, 2.722f, -311f), Quaternion.Euler(0f, 0f, 0f), new int[] {-1, -1, 12, 10}, new bool[] {false, false, false, false}),
+        new CamInfo(new Vector3(404.91f, 2.722f, -310.57f), Quaternion.Euler(0f, 90f, 0f), new int[] {-1, -1, 9, 11}, new bool[] {false, false, false, false}),
+        new CamInfo(new Vector3(405.99f, 2.722f, -310.18f), Quaternion.Euler(0f, 180f, 0f), new int[] {-1, -1, 10, 12}, new bool[] {false, false, false, false}),
+        new CamInfo(new Vector3(406.63f, 2.722f, -310.57f), Quaternion.Euler(0f, 270f, 0f), new int[] {-1, -1, 11, 9}, new bool[] {false, false, false, false}),
+        new CamInfo(new Vector3(404.03f, 2.722f, -305.16f), Quaternion.Euler(0f, 270f, 0f), new int[] {14, 18, -1, -1}, new bool[] {false, false, true, false}),
+        new CamInfo(new Vector3(400.07f, 2.722f, -305.16f), Quaternion.Euler(0f, 270f, 0f), new int[] {15, 17, -1, -1}, new bool[] {false, false, false, true}),
+        new CamInfo(new Vector3(394.45f, 2.722f, -305.57f), Quaternion.Euler(0f, 220f, 0f), new int[] {-1, -1, 16, -1}, new bool[] {false, false, false, false}),
+        new CamInfo(new Vector3(392.54f, 2.722f, -305.5f), Quaternion.Euler(0f, 90f, 0f), new int[] {17, -1, -1, 15}, new bool[] {false, false, true, false}),
+        new CamInfo(new Vector3(397.92f, 2.722f, -305.16f), Quaternion.Euler(0f, 90f, 0f), new int[] {18, 14, -1, -1}, new bool[] {false, true, false, false}),
+        new CamInfo(new Vector3(401.73f, 2.722f, -305.16f), Quaternion.Euler(0f, 90f, 0f), new int[] {19, 13, -1, -1}, new bool[] {true, false, false, false}),
+        new CamInfo(new Vector3(406.11f, 2.722f, -305.16f), Quaternion.Euler(0f, 90f, 0f), new int[] {-1, 8, -1, -1}, new bool[] {false, false, false, false}),
+        new CamInfo(new Vector3(390.24f, 2.722f, -306.25f), Quaternion.Euler(0f, 260f, 0f), new int[] {-1, -1, 21, 21}, new bool[] {false, false, false, false}),
+        new CamInfo(new Vector3(387.21f, 2.722f, -306.17f), Quaternion.Euler(0f, 80f, 0f), new int[] {-1, -1, 20, 20}, new bool[] {false, false, false, false}),
     };
     GameObject[] MoveButton = new GameObject[4];
+    GameObject[] Block = new GameObject[4];
     public float mvTime = 1.0f;
     public float fadeTime = 1.0f;
     Vector3 targetPos;
@@ -59,6 +75,8 @@ public class CameraMove : MonoBehaviour
         {
             MoveButton[i] = GameObject.Find("MoveButton_" + i);
             MoveButton[i].SetActive(ci[0].mb[i] >= 0);
+            Block[i] = GameObject.Find("block_" + i);
+            Block[i].SetActive(ci[0].block[i]);
         }
     }
 
@@ -78,6 +96,7 @@ public class CameraMove : MonoBehaviour
                 for (int i = 0; i < 4; i++)
                 {
                     MoveButton[i].SetActive(ci[cur].mb[i] >= 0);
+                    Block[i].SetActive(ci[cur].block[i]);
                 }
             }
         }
@@ -94,6 +113,7 @@ public class CameraMove : MonoBehaviour
                 for (int i = 0; i < 4; i++)
                 {
                     MoveButton[i].SetActive(ci[cur].mb[i] >= 0);
+                    Block[i].SetActive(ci[cur].block[i]);
                 }
                 StartCoroutine(CoFadeOut());
             }
@@ -115,7 +135,6 @@ public class CameraMove : MonoBehaviour
             isMove = true;
             timer = 0;
         }
-
     }
     
     public void FadeCam(int n)
